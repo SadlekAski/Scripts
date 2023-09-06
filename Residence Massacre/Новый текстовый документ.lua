af76a5b5fbd514493c82a5ef13f1b338b85aef45
SadlekAski <129619951+SadlekAski@users.noreply.github.com>
2023-09-06 21:21:18 +0800

    Add files via upload

---
+++ Residence Massacre/Новый текстовый документ.lua
@@ -0,0 +1,11 @@
+local function createHighlight(instance)
+   local highlight = Instance.new("Highlight");
+   highlight.FillColor = Color3.new(1,1,1);
+   highlight.FillTransparency = 0;
+   highlight.OutlineTransparency = 1;
+   highlight.DepthMode = "AlwaysOnTop";
+   highlight.Parent = instance;
+end
+
+
+createHighlight(workspace.Mutant);
