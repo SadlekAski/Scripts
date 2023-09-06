4982fa15c430f57ea5acce3538475c1b41489414
SadlekAski <129619951+SadlekAski@users.noreply.github.com>
2023-09-06 22:05:29 +0800

    Update Residence massacre aUTOFARM.lua

--- Residence Massacre/Residence massacre aUTOFARM.lua
+++ Residence Massacre/Residence massacre aUTOFARM.lua
@@ -10,16 +10,21 @@ local Part2 = Instance.new("Part")
     Part2.Transparency = 1
 end
 
+getgenv().AE = false
 if game.PlaceId == 14437001043 and game:IsLoaded() then
     print("Game Loaded")
     task.wait(1)
     LPC.CFrame = workspace.BloodmoonTP.OutHitbox.CFrame
     task.wait(1)
+    getgenv().AE = true
+
+    while AE == true do wait(1)
     LPC.CFrame = workspace.Lobbies:GetChildren()[8].PlayersNum.CFrame
     if not game:IsLoaded() then
         print("Waiting for game to load")
         game.Loaded:Wait()
     end
+end
 end
 
 if game.PlaceId == 14491437089 and game:IsLoaded() then
