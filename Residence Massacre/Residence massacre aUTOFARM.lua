c6c3d32cef3b3a9f66a0154e783ed2dd5b44e691
SadlekAski <129619951+SadlekAski@users.noreply.github.com>
2023-09-09 02:10:43 +0800

    Fixed Autofarm

--- Residence Massacre/Residence massacre aUTOFARM.lua
+++ Residence Massacre/Residence massacre aUTOFARM.lua
@@ -1,12 +1,14 @@
 local LPC = game.Players.LocalPlayer.Character.HumanoidRootPart
 local LP = game.Players.LocalPlayer
 
+-- I Dont have much knowledge so code is bad
+
 local function TPPart()
 local Part2 = Instance.new("Part")
     Part2.Parent = workspace
-    Part2.Name = "TPPart2"
+    Part2.Name = "TPPart21"
     Part2.Anchored = true
-    Part2.CFrame = CFrame.new(-79.644043, 24.7999821, -133.520752) + Vector3.new(0,2,0)
+    Part2.CFrame = CFrame.new(-59.644043, 20.7999821, -133.520752)
     Part2.Transparency = 1
 end
 
@@ -28,25 +30,34 @@ end
 end
 
 if game.PlaceId == 14491437089 and game:IsLoaded() then
-    task.wait(2)
+    
+    task.wait(1)
+    local Objective = tostring(game:GetService("Players").LocalPlayer.PlayerGui.DialogueUI.ObjectiveFrame.ObjectiveText.Text)
     local Part = {}
     spawn(function()
     task.wait(0.5)
     Part = TPPart()
+    end)
+    if Objective == "Go inside your house" then
+        print("Goin home, IM making prints for testing")
     LPC.CFrame = workspace.InsideHitbox.CFrame
-    task.wait(0.2)
-    LPC.CFrame = workspace.TPPart2.CFrame + Vector3.new(20,0,0)
-    task.wait(0.1)
-    LPC.Anchored = true
+    task.wait(4)
+    repeat
+    wait(1)
+    
+    fireclickdetector(workspace.Radio.ClickDetector)
+    LPC.CFrame = workspace.Radio.Main.CFrame
+    until game:GetService("ReplicatedStorage").GameState.Active.Value == true
+    task.wait(3)
+    LPC.CFrame = workspace.TPPart21.CFrame + Vector3.new(0, 2, 0)
+    elseif Objective == "Prepare for the night" then
+
+        LPC.CFrame = workspace.TPPart21.CFrame + Vector3.new(0, 2, 0)
     if not game:IsLoaded() then
         print("Waiting for game to load")
         game.Loaded:Wait()
     end
-end)
-end
+    end
+    end
 
-getgenv().Radio = true
 
-while Radio == true do wait(0.2)
-fireclickdetector(workspace.Radio.ClickDetector)
-end
