4d56ce562dcf240e6e418dcf1f0fc22ed95794a9
SadlekAski <129619951+SadlekAski@users.noreply.github.com>
2023-09-09 04:27:05 +0800

    Update Equip any ability.lua

--- Blade Ball/Equip any ability.lua
+++ Blade Ball/Equip any ability.lua
@@ -1,6 +1,154 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local workspace = game:GetService("Workspace")
+local players = game:GetService("Players")
+local localPlayer = players.LocalPlayer
+local runService = game:GetService("RunService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local LP = game.Players.LocalPlayer
 local LPC = LP.Character:FindFirstChild("Abilities")
 
+local Window = Rayfield:CreateWindow({
+   Name = "Blade Ball",
+   LoadingTitle = "Idk",
+   LoadingSubtitle = "Idk",
+   ConfigurationSaving = {
+      Enabled = false,
+      FolderName = "Idk",
+      FileName = "Idk"
+   },
+   Discord = {
+      Enabled = true,
+      Invite = "hNX8VxcjMF",
+      RememberJoins = true
+   },
+   KeySystem = false,
+   KeySettings = {
+      Title = "IDK",
+      Subtitle = "Key System",
+      Note = "nuh",
+      FileName = "??",
+      SaveKey = true,
+      GrabKeyFromSite = false,
+      Key = "Hello"
+   }
+})
+
+local Misc = Window:CreateTab("Misc", 13014546637)
+
+local Dash = Misc:CreateButton({
+   Name = "Dash",
+   Callback = function()
+local function AbilityValue2()
+local TruValue = Instance.new("StringValue")
+workspace:FindFirstChild("AbilityThingyk1212"):Remove()
+        TruValue.Parent = game:GetService("Workspace")
+        TruValue.Name = "AbilityThingyk1212"
+        TruValue.Value = "Dash" --Change to Use other ability
+end
+
+for i,v in pairs(LPC:GetChildren()) do
+
+
+for i,b in pairs(LPC:GetChildren()) do
+    local Ability = b
+    
+    if v.Enabled == true then
+        local EquippedAbility = v
+        local ChosenAbility = {}
+        spawn(function()
+        ChosenAbility = AbilityValue2()
+    end)
+
+    task.wait(0.05)
+        local AbilityValue = workspace.AbilityThingyk1212
+        if b.Name == AbilityValue.Value then
+
+            v.Enabled = false
+            b.Enabled = true
+    end
+end
+end
+end
+   end,
+})
+
+local Forcefield = Misc:CreateButton({
+   Name = "Forcefield",
+   Callback = function()
+local function AbilityValue2()
+local TruValue = Instance.new("StringValue")
+workspace:FindFirstChild("AbilityThingyk1212"):Remove()
+        TruValue.Parent = game:GetService("Workspace")
+        TruValue.Name = "AbilityThingyk1212"
+        TruValue.Value = "Forcefield" --Change to Use other ability
+end
+
+for i,v in pairs(LPC:GetChildren()) do
+
+
+for i,b in pairs(LPC:GetChildren()) do
+    local Ability = b
+    
+    if v.Enabled == true then
+        local EquippedAbility = v
+        local ChosenAbility = {}
+        spawn(function()
+        ChosenAbility = AbilityValue2()
+    end)
+
+    task.wait(0.05)
+        local AbilityValue = workspace.AbilityThingyk1212
+        if b.Name == AbilityValue.Value then
+
+            v.Enabled = false
+            b.Enabled = true
+    end
+end
+end
+end
+   end,
+})
+
+local Invisibility = Misc:CreateButton({
+   Name = "Invisibility",
+   Callback = function()
+local function AbilityValue2()
+local TruValue = Instance.new("StringValue")
+workspace:FindFirstChild("AbilityThingyk1212"):Remove()
+        TruValue.Parent = game:GetService("Workspace")
+        TruValue.Name = "AbilityThingyk1212"
+        TruValue.Value = "Invisibility" --Change to Use other ability
+end
+
+for i,v in pairs(LPC:GetChildren()) do
+
+
+for i,b in pairs(LPC:GetChildren()) do
+    local Ability = b
+    
+    if v.Enabled == true then
+        local EquippedAbility = v
+        local ChosenAbility = {}
+        spawn(function()
+        ChosenAbility = AbilityValue2()
+    end)
+
+    task.wait(0.05)
+        local AbilityValue = workspace.AbilityThingyk1212
+        if b.Name == AbilityValue.Value then
+
+            v.Enabled = false
+            b.Enabled = true
+    end
+end
+end
+end
+   end,
+})
+
+local Platform = Misc:CreateButton({
+   Name = "Platform",
+   Callback = function()
 local function AbilityValue2()
 local TruValue = Instance.new("StringValue")
 workspace:FindFirstChild("AbilityThingyk1212"):Remove()
@@ -32,3 +180,190 @@ for i,b in pairs(LPC:GetChildren()) do
 end
 end
 end
+   end,
+})
+
+local RagingDeflection = Misc:CreateButton({
+   Name = "Raging Deflection",
+   Callback = function()
+local function AbilityValue2()
+local TruValue = Instance.new("StringValue")
+workspace:FindFirstChild("AbilityThingyk1212"):Remove()
+        TruValue.Parent = game:GetService("Workspace")
+        TruValue.Name = "AbilityThingyk1212"
+        TruValue.Value = "Raging Deflection" --Change to Use other ability
+end
+
+for i,v in pairs(LPC:GetChildren()) do
+
+
+for i,b in pairs(LPC:GetChildren()) do
+    local Ability = b
+    
+    if v.Enabled == true then
+        local EquippedAbility = v
+        local ChosenAbility = {}
+        spawn(function()
+        ChosenAbility = AbilityValue2()
+    end)
+
+    task.wait(0.05)
+        local AbilityValue = workspace.AbilityThingyk1212
+        if b.Name == AbilityValue.Value then
+
+            v.Enabled = false
+            b.Enabled = true
+    end
+end
+end
+end
+   end,
+})
+
+local ShadowStep = Misc:CreateButton({
+   Name = "Shadow Step",
+   Callback = function()
+local function AbilityValue2()
+local TruValue = Instance.new("StringValue")
+workspace:FindFirstChild("AbilityThingyk1212"):Remove()
+        TruValue.Parent = game:GetService("Workspace")
+        TruValue.Name = "AbilityThingyk1212"
+        TruValue.Value = "Shadow Step" --Change to Use other ability
+end
+
+for i,v in pairs(LPC:GetChildren()) do
+
+
+for i,b in pairs(LPC:GetChildren()) do
+    local Ability = b
+    
+    if v.Enabled == true then
+        local EquippedAbility = v
+        local ChosenAbility = {}
+        spawn(function()
+        ChosenAbility = AbilityValue2()
+    end)
+
+    task.wait(0.05)
+        local AbilityValue = workspace.AbilityThingyk1212
+        if b.Name == AbilityValue.Value then
+
+            v.Enabled = false
+            b.Enabled = true
+    end
+end
+end
+end
+   end,
+})
+
+local SuperJump = Misc:CreateButton({
+   Name = "Super Jump",
+   Callback = function()
+local function AbilityValue2()
+local TruValue = Instance.new("StringValue")
+workspace:FindFirstChild("AbilityThingyk1212"):Remove()
+        TruValue.Parent = game:GetService("Workspace")
+        TruValue.Name = "AbilityThingyk1212"
+        TruValue.Value = "Super Jump" --Change to Use other ability
+end
+
+for i,v in pairs(LPC:GetChildren()) do
+
+
+for i,b in pairs(LPC:GetChildren()) do
+    local Ability = b
+    
+    if v.Enabled == true then
+        local EquippedAbility = v
+        local ChosenAbility = {}
+        spawn(function()
+        ChosenAbility = AbilityValue2()
+    end)
+
+    task.wait(0.05)
+        local AbilityValue = workspace.AbilityThingyk1212
+        if b.Name == AbilityValue.Value then
+
+            v.Enabled = false
+            b.Enabled = true
+    end
+end
+end
+end
+   end,
+})
+
+local Telekinesis = Misc:CreateButton({
+   Name = "Telekinesis",
+   Callback = function()
+local function AbilityValue2()
+local TruValue = Instance.new("StringValue")
+workspace:FindFirstChild("AbilityThingyk1212"):Remove()
+        TruValue.Parent = game:GetService("Workspace")
+        TruValue.Name = "AbilityThingyk1212"
+        TruValue.Value = "Telekinesis" --Change to Use other ability
+end
+
+for i,v in pairs(LPC:GetChildren()) do
+
+
+for i,b in pairs(LPC:GetChildren()) do
+    local Ability = b
+    
+    if v.Enabled == true then
+        local EquippedAbility = v
+        local ChosenAbility = {}
+        spawn(function()
+        ChosenAbility = AbilityValue2()
+    end)
+
+    task.wait(0.05)
+        local AbilityValue = workspace.AbilityThingyk1212
+        if b.Name == AbilityValue.Value then
+
+            v.Enabled = false
+            b.Enabled = true
+    end
+end
+end
+end
+   end,
+})
+
+local ThunderDash = Misc:CreateButton({
+   Name = "Thunder Dash",
+   Callback = function()
+local function AbilityValue2()
+local TruValue = Instance.new("StringValue")
+workspace:FindFirstChild("AbilityThingyk1212"):Remove()
+        TruValue.Parent = game:GetService("Workspace")
+        TruValue.Name = "AbilityThingyk1212"
+        TruValue.Value = "Thunder Dash" --Change to Use other ability
+end
+
+for i,v in pairs(LPC:GetChildren()) do
+
+
+for i,b in pairs(LPC:GetChildren()) do
+    local Ability = b
+    
+    if v.Enabled == true then
+        local EquippedAbility = v
+        local ChosenAbility = {}
+        spawn(function()
+        ChosenAbility = AbilityValue2()
+    end)
+
+    task.wait(0.05)
+        local AbilityValue = workspace.AbilityThingyk1212
+        if b.Name == AbilityValue.Value then
+
+            v.Enabled = false
+            b.Enabled = true
+    end
+end
+end
+end
+   end,
+})
