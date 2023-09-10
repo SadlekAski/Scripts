2dda23575a9faf384f176afe80f0c7eeb503ef4f
SadlekAski <129619951+SadlekAski@users.noreply.github.com>
2023-09-10 08:15:26 +0800

    Update Gui.lua

--- Hide/Gui.lua
+++ Hide/Gui.lua
@@ -17,8 +17,8 @@ local Window = Rayfield:CreateWindow({
    LoadingTitle = "Hide",
    LoadingSubtitle = "by SadlekAski",
    ConfigurationSaving = {
-      Enabled = false,
-      FolderName = "Idk",
+      Enabled = true,
+      FolderName = "HideConfig",
       FileName = "Idk"
    },
    Discord = {
@@ -39,31 +39,47 @@ local Window = Rayfield:CreateWindow({
 })
 
 local Main = Window:CreateTab("Main", 13014546637)
+local Player = Window:CreateTab("Player", 13014546637)
+local Multiplayerthing = Main:CreateSection("Multiplayer Thing")
 
 local RemoveVoteScreen = Main:CreateButton({
    Name = "Remove Starter Screen (Asking ready)",
    Callback = function()
+      Rayfield:Notify({
+   Title = "Description",
+   Content = "For multiplayer when you die,rejoined (When playign with friends (it keeps black screen even if you press ready)",
+   Duration = 60,
+   Image = 4483362458,
+   Actions = { -- Notification Buttons
+      Ignore = {
+         Name = "Okay!",
+         Callback = function()
+      end
+   },
+},
+})
    game:GetService("Players").LocalPlayer.PlayerGui.ScreenGui.readyornot.Visible = false
 end
 })
 
+local Wifithing = Main:CreateSection("Wifi Thing")
 local RepairWifi = Main:CreateButton({
    Name = "Repair Wifi",
    Callback = function()
    workspace.WifiRepair.Attachment.RemoteEvent:FireServer()
 end
 })
-
-local RepairWifi2 = Main:CreateButton({
-   Name = "Repair Wifi Bind (Button F)",
-   Callback = function()
-   game.Players.LocalPlayer:GetMouse().KeyDown:Connect(function(key)
- if key == "f" then
-     workspace.WifiRepair.Attachment.RemoteEvent:FireServer()
- end
-end)
-end
+local BindWifiRepair = Main:CreateKeybind({
+   Name = "Repair Wifi keybind (Set to your)",
+   CurrentKeybind = "P",
+   HoldToInteract = false,
+   Flag = "WifiKeybind", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Keybind)
+workspace.WifiRepair.Attachment.RemoteEvent:FireServer()
+   end,
 })
+
+local Coffee = Main:CreateSection("Coffee")
 
 local GetCoffee = Main:CreateButton({
    Name = "Take Coffee",
@@ -89,13 +105,13 @@ local RefillCoffee = Main:CreateButton({
 end
 })
 
-local RefillCoffee = Main:CreateButton({
-   Name = "Refill Coffee Bind (V) + Take coffee",
-   Callback = function()
-   
-   game.Players.LocalPlayer:GetMouse().KeyDown:Connect(function(key)
- if key == "v" then
-     character.HumanoidRootPart.CFrame = workspace:GetChildren()[55].Part.CFrame + Vector3.new(5,0,0)
+local RefillCoffeebind = Main:CreateKeybind({
+   Name = "Refill + Take Coffee bind",
+   CurrentKeybind = "L",
+   HoldToInteract = false,
+   Flag = "RefillCoffeeBind", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Keybind)
+character.HumanoidRootPart.CFrame = workspace:GetChildren()[55].Part.CFrame + Vector3.new(5,0,0)
      task.wait(0.2)
      fireproximityprompt(workspace:GetChildren()[55].Part.Attachment.ProximityPrompt)
    task.wait(1)
@@ -104,7 +120,32 @@ local RefillCoffee = Main:CreateButton({
    fireproximityprompt(workspace.cofetriger.Attachment.ProximityPrompt)
    task.wait(0.2)
    fireproximityprompt(workspace.cofetriger.Attachment.ProximityPrompt)
- end
-end)
-end
+   end,
 })
+
+local SpeedToggle = Player:CreateToggle({
+   Name = "Walkspeed",
+   CurrentValue = false,
+   Flag = "SpeedToggle", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Value)
+   SpeedToggling = Value
+   end,
+})
+   local SpeedSlider = Player:CreateSlider({
+   Name = "Walkspeed",
+   Range = {10, 40},
+   Increment = 10,
+   Suffix = "Speed",
+   CurrentValue = 0,
+   Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Value2)
+
+   while true do wait()
+   if SpeedToggling then
+   character.Humanoid.WalkSpeed = Value2
+   end
+   end
+   end,
+
+})
+
