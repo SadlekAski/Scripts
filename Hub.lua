6180ce4cee7689c6ef81a2f2bdfa31a4518ba985
SadlekAski <129619951+SadlekAski@users.noreply.github.com>
2023-09-15 00:34:45 +0800

    Create Hub.lua

---
+++ Hub.lua
@@ -0,0 +1,9 @@
+-- its just a hub that made for game scripts I made bruh, not sure if it works, cant test rn
+
+if game.PlaceId == "13772394625" then
+loadstring(game:HttpGet('https://raw.githubusercontent.com/SadlekAski/Scripts/main/Blade%20Ball/Equip%20any%20ability.lua'))()
+end
+
+if game.PlaceId == "13950003759" then
+loadstring(game:HttpGet('https://raw.githubusercontent.com/SadlekAski/Scripts/main/Difficulty%20Machine/Difficulty%20machine%20luck%20(1).lua'))()
+end
