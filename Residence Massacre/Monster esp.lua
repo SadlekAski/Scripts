1000a16351af4ac1b3c465daa20fa2c321e17ca7
SadlekAski <129619951+SadlekAski@users.noreply.github.com>
2023-09-14 17:16:52 +0800

    Update Monster esp.lua

--- Residence Massacre/Monster esp.lua
+++ Residence Massacre/Monster esp.lua
@@ -1,4 +1,6 @@
-local function createHighlight(instance)
+-- DO NOT USE
+
+--[[local function createHighlight(instance)
    local highlight = Instance.new("Highlight");
    highlight.FillColor = Color3.new(1,1,1);
    highlight.FillTransparency = 0;
@@ -9,3 +11,4 @@ end
 
 
 createHighlight(workspace.Mutant);
+]]
