d329ff8ea2fd2a82e98a492a4b2349d30b5e1a66
SadlekAski <129619951+SadlekAski@users.noreply.github.com>
2023-09-16 00:07:28 +0800

    Update hNX8VxcjMF.lua

--- hNX8VxcjMF.lua
+++ hNX8VxcjMF.lua
@@ -1,9 +1,6 @@
 -- its just a hub that made for game scripts I made bruh, not sure if it works, cant test rn
 -- Discord invite: discord.gg/hNX8VxcjMF
 
-if game.PlaceId == 13772394625 or game.PlaceId == 14732610803 then
-loadstring(game:HttpGet('https://raw.githubusercontent.com/SadlekAski/Scripts/main/Blade%20Ball/Equip%20any%20ability.lua'))()
-elseif game.PlaceId == 13950003759 then
-loadstring(game:HttpGet('https://raw.githubusercontent.com/SadlekAski/Scripts/main/Difficulty%20Machine/Difficulty%20machine%20luck%20(1).lua'))()
-end
+loadstring(game:HttpGet('https://raw.githubusercontent.com/SadlekAski/Scripts/main/Main%20Loadstring/MainLoadstring.lua'))()
+
 
