3af83ccfe9e24888c224e689a81e92daa86fc1be
SadlekAski <129619951+SadlekAski@users.noreply.github.com>
2023-09-15 15:38:31 +0800

    Update Hub.lua

--- Hub.lua
+++ Hub.lua
@@ -1,6 +1,6 @@
 -- its just a hub that made for game scripts I made bruh, not sure if it works, cant test rn
 
-if game.PlaceId == 13772394625 then
+if game.PlaceId == 13772394625 or game.PlaceId == 14732610803 then
 loadstring(game:HttpGet('https://raw.githubusercontent.com/SadlekAski/Scripts/main/Blade%20Ball/Equip%20any%20ability.lua'))()
 elseif game.PlaceId == 13950003759 then
 loadstring(game:HttpGet('https://raw.githubusercontent.com/SadlekAski/Scripts/main/Difficulty%20Machine/Difficulty%20machine%20luck%20(1).lua'))()
