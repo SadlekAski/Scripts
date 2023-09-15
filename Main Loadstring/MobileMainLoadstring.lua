8b5c3d7e3eec2274feea882a9c85d3e5cc65db4e
SadlekAski <129619951+SadlekAski@users.noreply.github.com>
2023-09-16 02:21:05 +0800

    Create MobileMainLoadstring.lua

---
+++ Main Loadstring/MobileMainLoadstring.lua
@@ -0,0 +1,9 @@
+-- its just a hub that made for game scripts I made bruh, not sure if it works, cant test rn
+-- Discord invite: discord.gg/hNX8VxcjMF
+local Placeid = game.PlaceId
+
+if Placeid == 13772394625 or Placeid == 14732610803 then -- Blade Ball
+loadstring(game:HttpGet('https://raw.githubusercontent.com/SadlekAski/Scripts/main/Blade%20Ball/MobileGui.lua'))()
+elseif Placeid == 13950003759 then -- Difficulty Machine
+loadstring(game:HttpGet('https://raw.githubusercontent.com/SadlekAski/Scripts/main/Difficulty%20Machine/Difficulty%20machine%20luck%20(1).lua'))()
+end
