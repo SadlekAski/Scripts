016097cf38d43501ff310634229d7cf9ae00c47f
SadlekAski <129619951+SadlekAski@users.noreply.github.com>
2023-09-16 00:13:40 +0800

    Update MainLoadstring.lua

--- Main Loadstring/MainLoadstring.lua
+++ Main Loadstring/MainLoadstring.lua
@@ -1,8 +1,9 @@
 -- its just a hub that made for game scripts I made bruh, not sure if it works, cant test rn
 -- Discord invite: discord.gg/hNX8VxcjMF
+local Placeid = game.PlaceId
 
-if game.PlaceId == 13772394625 or game.PlaceId == 14732610803 then -- Blade Ball
+if Placeid == 13772394625 or Placeid == 14732610803 then -- Blade Ball
 loadstring(game:HttpGet('https://raw.githubusercontent.com/SadlekAski/Scripts/main/Blade%20Ball/Equip%20any%20ability.lua'))()
-elseif game.PlaceId == 13950003759 then -- Difficulty Machine
+elseif Placeid == 13950003759 then -- Difficulty Machine
 loadstring(game:HttpGet('https://raw.githubusercontent.com/SadlekAski/Scripts/main/Difficulty%20Machine/Difficulty%20machine%20luck%20(1).lua'))()
 end
