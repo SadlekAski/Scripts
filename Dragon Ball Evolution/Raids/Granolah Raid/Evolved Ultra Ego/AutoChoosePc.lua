fb3692be38d5a7d7320d70f41f1e86a9955081c0
SadlekAski <129619951+SadlekAski@users.noreply.github.com>
2023-10-04 17:53:22 +0800

    Update AutoChoosePc.lua

--- Dragon Ball Evolution/Raids/Granolah Raid/Evolved Ultra Ego/AutoChoosePc.lua
+++ Dragon Ball Evolution/Raids/Granolah Raid/Evolved Ultra Ego/AutoChoosePc.lua
@@ -1,16 +1,21 @@
-if game:IsLoaded() then
-
-
-task.wait(3)
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
 
 local function guiclick(a)
     game:GetService("VirtualInputManager"):SendMouseButtonEvent(a.AbsolutePosition.X+a.AbsoluteSize.X/2,a.AbsolutePosition.Y+50,0,true,a,1)
     game:GetService("VirtualInputManager"):SendMouseButtonEvent(a.AbsolutePosition.X+a.AbsoluteSize.X/2,a.AbsolutePosition.Y+50,0,false,a,1)
 end
 
-while true do wait()
-guiclick(game:GetService("Players").LocalPlayer.PlayerGui.TeleportUI:FindFirstChild("ClosingFrame"):FindFirstChild("Buttons"):FindFirstChild("PC"))
-end
-elseif not game:IsLoaded() then
-    game.Loaded:Wait()
-end
+local PCChecker = Instance.new("BoolValue")
+PCChecker.Parent = workspace
+PCChecker.Name = "PCChecker"
+PCChecker.Value = false
+
+repeat wait()
+    if game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("TeleportUI") then
+        guiclick(game:GetService("Players").LocalPlayer.PlayerGui.TeleportUI:WaitForChild("ClosingFrame"):WaitForChild("Buttons"):WaitForChild("PC"))
+    else
+        workspace.PCChecker.Value = true
+    end
+until workspace.PCChecker.Value == true
