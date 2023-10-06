376b45c0a9c588f96d4642e697e695fa6f5cd38f
SadlekAski <129619951+SadlekAski@users.noreply.github.com>
2023-10-07 04:01:56 +0800

    Update AutoCollectShards.lua

--- Dragon Ball Evolution/Open World/AutoCollectShards.lua
+++ Dragon Ball Evolution/Open World/AutoCollectShards.lua
@@ -4,10 +4,28 @@ local character = localplayer.Character
 local croot = character.HumanoidRootPart
 getgenv().ShardTpTPShard = true
 
+local function customtwn(input, studspersecond, offset)
+    local char = game:GetService("Players").LocalPlayer.Character;
+    local input = input or error("input is nil");
+    local studspersecond = studspersecond or 1000;
+    local offset = offset or CFrame.new(0,0,0);
+    local vec3, cframe;
+ 
+    if typeof(input) == "table" then
+        vec3 = Vector3.new(unpack(input)); cframe = CFrame.new(unpack(input));
+    elseif typeof(input) ~= "Instance" then
+        return error("wrong format used");
+    end;
+    
+    Time = (char.HumanoidRootPart.Position - (vec3 or input.Position)).magnitude/studspersecond;
+    local twn = game.TweenService:Create(char.HumanoidRootPart, TweenInfo.new(Time,Enum.EasingStyle.Linear), {CFrame = (cframe or input.CFrame) * offset});
+    twn:Play();
+    twn.Completed:Wait();
+ end;
+
 while ShardTpTPShard == true do task.wait(0.5)
 for i,v in ipairs(shards:GetChildren()) do
-v.Name = "Shard" .. i
 
-croot.CFrame = v.CFrame
+    customtwn(v, 100, CFrame.new(0,0,0));
 end
 end
